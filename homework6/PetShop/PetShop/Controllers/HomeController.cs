a8c61464bd1a3f833037f04dd22020c599302bf4
annakelley <annakelley4@gmail.com>
2014-03-18 23:01:00 -0400

    hmwk 6 finished

--- homework6/PetShop/PetShop/Controllers/HomeController.cs
+++ homework6/PetShop/PetShop/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,6 +21,20 @@ namespace PetShop.Controllers
         {
 
             return View(db.Pets.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pets.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
         }
     }
 }
