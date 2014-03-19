109e65f161b9eee824f428fe7a754add1a23a036
annakelley <annakelley4@gmail.com>
2014-03-18 20:03:03 -0400

    hmwk 6 home page set up and start of manage pg

--- homework6/PetShop/PetShop/Controllers/HomeController.cs
+++ homework6/PetShop/PetShop/Controllers/HomeController.cs
@@ -13,16 +13,8 @@ namespace PetShop.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Inventory()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
